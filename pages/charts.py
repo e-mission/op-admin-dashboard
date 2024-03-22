78915f14ec344b3bdc9897876562676b90254e56
Asmita Acharya <79387860+achasmita@users.noreply.github.com>
2024-03-22 16:23:17 -0700

    Added dropdown to switch between data for Pygwalker page

--- pages/charts.py
+++ pages/charts.py
@@ -12,24 +12,42 @@ intro = """## Charts"""
 
 
 layout = html.Div([
-   html.H1('Testing...'),
-   html.Iframe(id='trips-charts', srcDoc = '', style={'width':'100vw', 'height':'100vh'})
+   html.H1('PygWalker Charts'),
+   dcc.Dropdown(
+      id='data-options',
+      options=[
+         {'label': 'store_trips', 'value': 'store_trips'},
+         {'label': 'store_user_stats', 'value': 'store_user_stats'}
+      ],
+      value = 'store_trips'
+   ),
+   html.Iframe(id='charts', srcDoc = '', style={'width':'100vw', 'height':'100vh'})
 ])
 
 @callback(
-   Output('trips-charts', 'srcDoc'),
-   # Input('store-user-stats', 'data'),
+   Output('charts', 'srcDoc'),
+   Input('store-user-stats', 'data'),
    Input('store-trips', 'data'),
+   Input('data-options', 'value')
 )
-def generate_charts(store_trips):
-   if not store_trips:
-      return no_update
-   data = store_trips["data"]
-   columns = perm_utils.get_allowed_trip_columns()
-   columns.update(
-         col['label'] for col in perm_utils.get_allowed_named_trip_columns()
-      )
-   has_perm = perm_utils.has_permission('data_trips')
+def generate_charts(store_user_stats, store_trips, selected_option):
+   if selected_option == 'store_trips':
+      if not store_trips:
+         return no_update
+      data = store_trips["data"]
+      columns = perm_utils.get_allowed_trip_columns()
+      columns.update(
+            col['label'] for col in perm_utils.get_allowed_named_trip_columns()
+         )
+      has_perm = perm_utils.has_permission('data_trips')
+
+   if selected_option == 'store_user_stats':
+      if not store_user_stats:
+         return no_update
+      data = store_user_stats["data"]
+      columns = perm_utils.get_uuids_columns()
+      has_perm = perm_utils.has_permission('data_uuids')
+   
    if not has_perm:
       return no_update
    df = pd.DataFrame(data)
