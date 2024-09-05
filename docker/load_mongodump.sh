35ea51fba3d9a3886fe336fa054c2c1045c6d50f
TeachMeTW <robin@robinttw.com>
2024-09-05 15:40:34 -0700

    Modified Script

--- docker/load_mongodump.sh
+++ docker/load_mongodump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <dump_file> <database_name> <docker_container_name>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <dump_file> <database_name> <docker_container_name> <collection_name>"
     exit 1
 fi
 
@@ -10,19 +10,20 @@ fi
 MONGODUMP_FILE=$1
 DATABASE_NAME=$2
 DOCKER_CONTAINER_NAME=$3
+COLLECTION_NAME=$4
 
 # Extract the base name of the dump file for use in the restore command
-FILE_NAME=$(basename $MONGODUMP_FILE)
+FILE_NAME=$(basename "$MONGODUMP_FILE")
 
 # Drop the existing database to ensure it’s clean before restoring the new dump
 echo "Dropping the existing database $DATABASE_NAME"
-docker exec $DOCKER_CONTAINER_NAME mongo $DATABASE_NAME --eval "db.dropDatabase()"
+docker exec "$DOCKER_CONTAINER_NAME" mongo "$DATABASE_NAME" --eval "db.dropDatabase()"
 
 # Copy the MongoDB dump file from the local machine to the Docker container
 echo "Copying file to Docker container $DOCKER_CONTAINER_NAME"
-docker cp $MONGODUMP_FILE $DOCKER_CONTAINER_NAME:/tmp
+docker cp "$MONGODUMP_FILE" "$DOCKER_CONTAINER_NAME:/tmp"
 
 # Restore the dump into the specified database
 echo "Restoring the dump from $FILE_NAME to database $DATABASE_NAME"
-docker exec -e MONGODUMP_FILE=$FILE_NAME $DOCKER_CONTAINER_NAME bash -c \
-    'cd /tmp && tar xvf $MONGODUMP_FILE && mongorestore -d '"$DATABASE_NAME"' dump/openpath_prod_ca_ebike'
+docker exec -e MONGODUMP_FILE="$FILE_NAME" "$DOCKER_CONTAINER_NAME" bash -c \
+    'cd /tmp && tar xvf "$MONGODUMP_FILE" && mongorestore -d '"$DATABASE_NAME"' dump/'"$COLLECTION_NAME"''
